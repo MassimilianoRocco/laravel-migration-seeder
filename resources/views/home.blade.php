b4b9616cc64b23f792e997d87c0d5dace7db0713
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-07-04 09:24:33 +0200

    Added: View

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.app')
 
+@section('title', "BoolLine")
+@section('css')
+    <link rel="stylesheet" href="{{ asset('resources/scss/app.css') }}">
+@endsection
+
 @section('content')
-    <h1>Dati da "config/data.php": {{ $home }}</h1>
-    <p>This is my paragraph content.</p>
+    <h1>TRAINS LIST</h1>
+
+    @foreach ($trains as $train)
+    <div class="card">
+        <p>Azienda: {{$train->azienda}}</p>
+        <p>Stazione di partenza: {{$train->stazione_di_partenza}}</p>
+        <p>Stazione di arrivo: {{$train->stazione_di_arrivo}}</p>
+        <p>Orario di Partenza: {{$train->orario_di_partenza}}</p>
+        <p>Orario di Arrivo: {{$train->orario_di_arrivo}}</p>
+        <p>Codice Treno: {{$train->codice_treno}}</p>
+        <p>Numero Carrozze: {{$train->numero_Carrozze}}</p>
+        <p>In orario? {{$train->in_Orario}}</p>
+        <p>Cancellato? {{$train->cancellato}}</p>
+        <p>Treno Veloce?{{$train->treno_veloce}}</p>
+    </div>
+    @endforeach
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield("title", "Hello Laravel World")</title>
     @vite('resources/js/app.js')
+    @yield('css')
 </head>
 <body>
 
